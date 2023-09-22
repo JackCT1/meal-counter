d26d1e19904f36bc30c71d61fa204edf178cc1c0
JackCT1 <jackthomas933@gmail.com>
2023-09-22 15:20:40 +0100

    meal name prop passed down

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import Meal from "./components/meal";
 
 function App() {
+  const meal = "Vegetarian";
   return (
     <>
       <h1>Meal Counts</h1>
-      <Meal></Meal>
+      <Meal mealName={meal} />
     </>
   );
 }
