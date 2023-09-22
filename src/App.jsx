157fb078b51d32f8379be1fba2ca7412311f1c33
JackCT1 <jackthomas933@gmail.com>
2023-09-22 15:44:52 +0100

    rendering components for multiple meal types

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import Meal from "./components/meal";
 
 function App() {
   const meal = "Vegetarian";
+  const mealTypes = [
+    "Vegetarian",
+    "Vegan",
+    "Halal",
+    "Gluten-free",
+    "Anything!",
+  ];
+  const mealComponents = [];
+  {
+    mealTypes.forEach((meal) => {
+      mealComponents.push(<Meal mealName={meal} />);
+    });
+  }
   return (
     <>
       <h1>Meal Counts</h1>
-      <Meal mealName={meal} />
+      {mealComponents}
     </>
   );
 }
